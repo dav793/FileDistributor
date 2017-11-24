e09604e88598999d4ec0e0e0ced599b4b5dfa4bb
David Vargas <dav793@gmail.com>
2017-11-23 22:37:18 -0600

    Restore config directory

---
+++ config/dirPaths.template.js
@@ -0,0 +1,4 @@
+module.exports = {
+    podcasts: 'C:/Users/David/Documents/podcasts',
+	books: 'C:/Users/David/Documents/books'
+};
---
+++ config/environment.template.js
@@ -0,0 +1,4 @@
+module.exports = {
+    IPADDR: '192.168.0.134',
+    PORT: 80
+};
